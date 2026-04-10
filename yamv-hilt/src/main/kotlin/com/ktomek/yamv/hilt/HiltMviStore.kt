8bac6c603c76f3e8138b32fb6dcc8474673025b4
Tomasz Kaszkowiak <tomasz.kaszkowiak@gmail.com>
2026-04-10 10:40:54 +0200

    [#13] refactor: update HiltMviStore to use MviRetainedStore

--- yamv-hilt/src/main/kotlin/com/ktomek/yamv/hilt/HiltMviStore.kt
+++ yamv-hilt/src/main/kotlin/com/ktomek/yamv/hilt/HiltMviStore.kt
@@ -2,13 +2,13 @@ package com.ktomek.yamv.hilt
 
 import androidx.compose.runtime.Composable
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.ktomek.yamv.viewmodel.MviViewModel
+import com.ktomek.yamv.viewmodel.MviRetainedStore
 
 /**
- * Returns a Hilt-provided [MviViewModel] instance scoped to the current backstack entry.
+ * Returns a Hilt-provided [MviRetainedStore] instance scoped to the current backstack entry.
  *
  * Prefer this over calling [hiltViewModel] directly so call sites stay decoupled from
- * the Hilt navigation API and the type constraint on [MviViewModel] is enforced.
+ * the Hilt navigation API and the type constraint on [MviRetainedStore] is enforced.
  *
  * Usage:
  * ```
@@ -17,4 +17,4 @@ import com.ktomek.yamv.viewmodel.MviViewModel
  * ```
  */
 @Composable
-inline fun <reified VM : MviViewModel<*, *>> hiltMviStore(): VM = hiltViewModel()
+inline fun <reified VM : MviRetainedStore<*, *>> hiltMviStore(): VM = hiltViewModel()
